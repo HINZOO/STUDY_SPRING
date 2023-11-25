14b2a0691a9c611f435550287379cc8332689568
HINZOO <hinge1128@gmail.com>
2023-11-25 13:33:10 +0900

    컴포넌트 스캔과 자동 의존과계 설정

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -2,14 +2,17 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service // 서비스 안에 Component 가 존재, 실제로 이것은 Component 이다.
 public class MemberService {
 
     private final MemberRepository memberRepository;
-
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
