f51d408a75800b32bffeb5337f68aaa9bc6557e1
HINZOO <hinge1128@gmail.com>
2023-11-26 12:51:43 +0900

    ãjpa

--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,14 @@
 package hello.hellospring.domain;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Member {
+
+    @Id  @GeneratedValue(strategy = GenerationType.IDENTITY)//DB가 ID를 자동생성하는걸 IDENTITY 라 부름.
     private Long id;
+
+    @Column(name="name")//DB의 컬럼명이 다를때 기술
     private String name;
     public Long getId() {
         return id;
